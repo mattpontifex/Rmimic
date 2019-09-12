a2f24a17abc980762d4f883767ee235c1fdc18f4
mattpontifex <31024683+mattpontifex@users.noreply.github.com>
2019-09-12 18:13:20 -0400

    Test revisions

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,2 +1,2 @@
-#tryCatch(library(testthat), error=function(e){install.packages("testthat"); library(testthat)}) # test_check
+tryCatch(library(testthat), error=function(e){install.packages("testthat"); library(testthat)}) # test_check
 test_check("Rmimic")
---
+++ tests/testthat/test-fuzzyP.R
@@ -0,0 +1,11 @@
+context("fuzzyP")
+
+test_that("fuzzyP works", {
+  
+  outPvalue <- fuzzyP(0.04)
+  
+  expect_equal(outPvalue$interpret, 0.04)
+  expect_equal(outPvalue$report, 0.04)
+  expect_equal(outPvalue$modifier, "=")
+  
+})
