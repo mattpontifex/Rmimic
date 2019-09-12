164f64505604b2eb1115c674e3a2c4140bd33320
mattpontifex <31024683+mattpontifex@users.noreply.github.com>
2019-09-12 18:14:29 -0400

    fuzzyp

---
+++ R/fuzzyP.R
@@ -0,0 +1,64 @@
+#' fuzzyP
+#'
+#' @description Function to properly round p values. Returns several values allowing \cr the user to choose what rounded value is appropriate.
+#'
+#' @param datain Significance value.
+#'
+#' @return A list with the following elements:
+#' \item{interpret}{Rounded p value for use in interpretations.}
+#' \item{report}{Rounded p value for use in summary reports.}
+#' \item{exact}{Exact p value.}
+#' \item{modifier}{Equals sign unless the p value is less than .001.}
+#'
+#' @author Matthew B. Pontifex, \email{pontifex@@msu.edu}, July 28, 2017
+#'
+#' @examples
+#'     # Round a randomly generated number between 0 and 1.
+#'     outPvalue <- fuzzyP(stats::runif(1))
+#'
+#' @export
+
+fuzzyP <- function(datain) {
+
+ modifier <- "="
+ if (!is.na(datain)) {
+   report <- round(datain, digits=3)
+   interpret <- report
+    if (datain < 0.001) {
+      interpret <- 0.001
+      report <- interpret
+      modifier <- "<"
+    } else if (datain < 0.01) {
+      interpret <- round(datain, digits=3)
+    } else if (datain == 0.01) {
+      interpret <- datain
+    } else if (datain == 0.025) {
+      interpret <- datain
+    } else if (datain < 0.05) {
+      interpret <- round(datain, digits=3)
+    } else if (round(datain, digits=3) < 0.0551) {
+      interpret <- floor(datain * 100) / 100
+    } else if (datain < 0.06) {
+      interpret <- round(datain, digits=3)
+    } else if (datain < 0.1) {
+      interpret <- round(datain, digits=2)
+    } else {
+      interpret <- round(datain, digits=1)
+    }
+   if (interpret == 1) {
+     interpret <- round(datain, digits=2)
+   }
+ } else {
+   report <- NA
+   interpret <- 1
+ }
+
+ res <- list()
+ res$interpret <- interpret
+ res$report <- report
+ res$exact <- datain
+ res$modifier <- modifier
+ class(res)
+
+ return(res)
+}
